127c23eb40588263bcebf97b02ae48e13c508b0e
«alexander <favorite@bk.ru>
2022-08-21 14:31:27 +0300

    hw3

---
+++ hw_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine());
+
+
+if (a%2 == 0)
+{
+Console.WriteLine("Четное число");
+}
+else 
+{
+Console.WriteLine("Нечетное число");
+}
+
+
