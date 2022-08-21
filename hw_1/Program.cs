3437023b95c54d05069c9c99ef4465b708eae686
«alexander <favorite@bk.ru>
2022-08-21 15:04:27 +0300

    hw1_2

--- hw_1/Program.cs
+++ hw_1/Program.cs
@@ -1,9 +1,25 @@
-﻿Console.WriteLine("Введите первое число");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int b = int.Parse(Console.ReadLine());
 int max = a;
-if (max < b) max = b;
+int min = b;
+
+if (b > max) 
+{
+    max = b;
+    min = a;
+}
+else 
+    if (b < max)
+{
+    max = a;
+    min = b;
+}
+
 
 Console.Write("Максимальное число ");
 Console.WriteLine(max);
+Console.Write("Минимальное число ");
+Console.WriteLine(min);
